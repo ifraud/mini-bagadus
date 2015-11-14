7bf65b08ba48223441863481000b507663376e7b
ifraud <gaddam.vamsidhar@gmail.com>
2015-11-15 00:54:54 +0100

    Successfully access sensor

--- main.cpp
+++ main.cpp
@@ -8,7 +8,17 @@ int main(int argc, char *argv[])
     int numDevs;
 
     is_GetNumberOfCameras(&numDevs);
-    std::cout<<"Hellow how are "<<numDevs<<std::endl;
+    std::cout<<"The number of cameras are "<<numDevs<<std::endl;
+
+    HIDS m_Cam = 0;
+    SENSORINFO m_sInfo;
+    int nRet = is_InitCamera(&m_Cam, NULL);
+    if(nRet == IS_SUCCESS){
+        //Camera Opened succesfully
+        is_GetSensorInfo(m_Cam,&m_sInfo);
+        std::cout<<"The sensor is "<<m_sInfo.strSensorName<<std::endl;
+    }
+
 
     return a.exec();
 }
